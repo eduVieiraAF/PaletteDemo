8130c795fbf04a37b80b8521d2126fb25635835b
Eduardo Vieira <edu_vieira@hotmail.com>
2022-07-26 09:47:36 -0300

    Code:
    → using url instead of PNG;
    → Code lines for Glide and Palette;
    → Internet permission in Manifest.

--- app/src/main/java/com/example/palettedemo/MainActivity.kt
+++ app/src/main/java/com/example/palettedemo/MainActivity.kt
@@ -1,11 +1,62 @@
 package com.example.palettedemo
 
+import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.graphics.drawable.toBitmap
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val clMain = findViewById<ConstraintLayout>(R.id.cl_main)
+        val ivMain = findViewById<ImageView>(R.id.iv_main)
+
+        Glide.with(this)
+            .load("https://cdn.pixabay.com/photo/2018/05/03/21/49/android-3372580_960_720.png")
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    Log.i("FAILURE", "Failed loading image")
+
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    Palette.from(resource!!.toBitmap())
+                        .generate{
+                            palette ->
+                            palette?.let {
+                                val intColor = it.vibrantSwatch?.rgb ?: 0
+
+                                clMain.setBackgroundColor(intColor)
+                            }
+                        }
+                    return false
+                }
+            })
+            .into(ivMain)
     }
 }
